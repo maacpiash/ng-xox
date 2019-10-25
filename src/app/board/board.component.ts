7821dbe98c55e290d8b2b7cd6acf3d7d37c539be
Ahad Chowdhury <maac@pia.sh>
2019-10-26 03:43:34 +0600

    Minor changes
    - 'Who the winner is' is now a boolean, instead of a string.
    - The board now disappears when game is over!
    - Rearrange the 3D array in calculation function.
    - Fix the title of the page.
    
    @maacpiash

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class BoardComponent implements OnInit {
   squares: any[];
   xIsNext: boolean;
-  winner: string;
+  xIsWinner: boolean;
+  gameOver: boolean;
 
   constructor() { }
 
@@ -18,8 +19,9 @@ export class BoardComponent implements OnInit {
 
   newGame() {
     this.squares = Array(9).fill(null);
-    this.winner = null;
     this.xIsNext = true;
+    this.xIsWinner = null;
+    this.gameOver = false;
   }
 
   get player() {
@@ -32,26 +34,21 @@ export class BoardComponent implements OnInit {
       this.xIsNext = !this.xIsNext;
     }
 
-    this.winner = this.calculateWinner(idx);
+    this.xIsWinner = this.calculatexIsWinner(idx);
   }
 
-  calculateWinner(i: number) {
+  calculatexIsWinner(i: number) {
     const lines = [
-      [[0, 1, 2], [0, 3, 6], [0, 4, 8]],
-      [[0, 1, 2], [1, 4, 7]],
-      [[0, 1, 2], [2, 5, 8], [2, 4, 6]],
-      [[3, 4, 5], [0, 3, 6]],
-      [[3, 4, 5], [1, 4, 7], [0, 4, 8], [2, 4, 6]],
-      [[3, 4, 5], [2, 5, 8]],
-      [[6, 7, 8], [0, 3, 6], [2, 4, 6]],
-      [[6, 7, 8], [1, 4, 7]],
-      [[6, 7, 8], [2, 5, 8], [0, 4, 8]]
+      [[0, 1, 2], [0, 3, 6], [0, 4, 8]], [[0, 1, 2], [1, 4, 7]], [[0, 1, 2], [2, 5, 8], [2, 4, 6]], // 0  1  2
+      [[3, 4, 5], [0, 3, 6]], [[3, 4, 5], [1, 4, 7], [0, 4, 8], [2, 4, 6]], [[3, 4, 5], [2, 5, 8]], // 3  4  5
+      [[6, 7, 8], [0, 3, 6], [2, 4, 6]], [[6, 7, 8], [1, 4, 7]], [[6, 7, 8], [2, 5, 8], [0, 4, 8]]  // 6  7  8
     ];
 
     for (const line of lines[i]) {
       const [a, b, c] = line;
       if (this.squares[a] && this.squares[a] === this.squares[b] && this.squares[a] === this.squares[c]) {
-        return this.squares[a];
+        this.gameOver = true;
+        return this.squares[a] === 'X';
       }
     }
     return null;
